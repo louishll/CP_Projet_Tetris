68af0a085906ec1dd0f7972571deb37136baa5e1
unknown <louis.holle@etu.univ-poitiers.fr>
2023-03-06 17:47:39 +0100

    séance 1 test

--- tests_jeuCP2.ml
+++ tests_jeuCP2.ml
@@ -1,12 +1,39 @@
+(*
 open CPinter;;
 open JeuCP2;;
+ *)
 
 (* ---------------------------- *)
 (* test de : convert            *)
 (* ---------------------------- *)
 
 (* *)
+let test_convert_fonctional_1(status: t_test_status): unit =
+  let test_step : t_test_step =
+    test_start(status, "convert_fonctional_1")
+  and p : t_point = {x = 2; y = 2}
+  and p_sorti : t_point = {x = 100; y = 100} in
+  let test_result : t_point t_test_result = test_exec(test_step, convert, (p, {x = 0; y = 0}, dilat)) in
+  (
+    if test_is_success(test_result)
+    then assert_equals(test_step, "carré (2;2)", test_get(test_result), p_sorti)
+    else test_error(test_step);
+    test_end(test_step)
+  )
+;;
+(* auteur : Louis *)
 
+let test_draw_absolute_pt_functional_1(status: t_test_status) : unit =
+  let test_step : t_test_step =
+    test_start(status, "draw_absolute_pt_functional_1")
+  let test_result : t_point t_test_result = test_exec(test_step, convert, (p, {x = 0; y = 0}, dilat)) in
+  (
+    if test_is_success(test_result)
+    then assert_equals(test_step, "carré (2;2)", test_get(test_result), p_sorti)
+    else test_error(test_step);
+    test_end(test_step)
+  )
+;;
 
 (* ---------------------------- *)
 (*     fonction de test         *)
@@ -15,6 +42,9 @@ open JeuCP2;;
 let test_run() : unit =
   let alltests : t_test_status = create_test_status() in
   (
-    
+    test_convert_fonctional_1(alltests);
+
+    (* print des resultats de test (DOIT RESTER A LA FIN !!!) *)
+    print_test_report(alltests)
   )
 ;;

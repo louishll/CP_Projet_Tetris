b6e158e4cd0b8dc24c20493d7ed88a5df2d97a75
Nicolasbouillopro <nicolas.bouillon.pro1@gmail.com>
2023-03-09 10:55:35 +0100

    drawfillabsolutept_test

--- tests_jeuCP2.ml
+++ tests_jeuCP2.ml
@@ -1,6 +1,7 @@
 (*
 open CPinter;;
 open JeuCP2;;
+
  *)
 
 (* ---------------------------- *)
@@ -75,7 +76,31 @@ let test_fill_absolute_pt_functional_1(status: t_test_status) : unit =
   )
 ;;
 (*auteurs : mélie et louis*)
+(* ---------------------------- *)
+(* test de :drawfill_absolute_pt*)
+(* ---------------------------- *)
 
+let test_drawfill_absolute_pt_functional_1(status: t_test_status) : unit =
+  let test_step : t_test_step = test_start(status, "fill_absolute_pt_functional_1")
+  and p : t_point = {x = 2; y = 2} in
+  let test_result : t_point t_test_result = test_exec(test_step, convert, (p, {x = 0; y = 0}, dilat)) in
+  (
+    if test_is_success(test_result)
+    then
+      (
+        open_graph(320, 610);
+        fill_rect(50, 50, 10, 450);
+        fill_rect(260, 50, 10, 450);
+        fill_rect(50, 50, 220, 10);
+        drawfill_absolute_pt(p, {x = 0; y = 0}, dilat, blue);
+        print_string("Voyez vous un carré bleu avec une bordure noir en (2;2) (oui/non)");
+        let reponse : string = read_line() in
+        assert_equals(test_step, "carré (2;2)", reponse, "oui")
+      )
+    else test_error(test_step);
+    test_end(test_step)
+  )
+;;
 (* ---------------------------- *)
 (*     fonction de test         *)
 (* ---------------------------- *)
